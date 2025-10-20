932baa6c59fecc29b1a277993eda288b105800f0
Kezaldius <0kezald0@gmail.com>
2025-10-20 13:23:44 +0300

    fixed category post method

--- api/views.py
+++ api/views.py
@@ -1,5 +1,6 @@
 from api import app
 from flask import request, jsonify
+from datetime import datetime
 import uuid
 
 users = {}
@@ -43,12 +44,15 @@ def delete_user(user_id):
 @app.route('/category', methods = ['POST'])
 def create_categoires():
     category_data = request.get_json()
-    category_id = str(uuid.uuid4)
+    category_id = str(uuid.uuid4())
 
     category = {
         "id": category_id,
         "Назва категорії": category_data.get("Назва категорії")
     }
+
+    categories[category_id] = category
+    return jsonify(category), 201
 
 @app.route('/category', methods = ['GET'])
 def get_categories():
@@ -59,4 +63,19 @@ def delete_category(category_id):
     del categories[category_id]
     return '', 204
 
+@app.route('/record', methods = ['POST'])
+def create_record():
+    record_data = request.get_json()
+    record_id = str(uuid.uuid4())
+
+    record = {
+        "id": record_id,
+        "Id користувача": record_data.get("user_id"),
+        "Id категорії": record_data.get("category_id"),
+        "Дата та час створення запису": datetime.timezone.utc().isoformat(),
+        "Сума витрати": record_data.get("Сума витрати")
+    }
+
+    records[record_id] = record
+    return jsonify(record), 201
 
