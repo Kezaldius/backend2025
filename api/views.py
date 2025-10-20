227e257398cd87264582a780caabcd88b7d5ac2a
Kezaldius <0kezald0@gmail.com>
2025-10-20 13:00:39 +0300

    Created user CRUD

--- api/views.py
+++ api/views.py
@@ -1,5 +1,41 @@
 from api import app
+from flask import request, jsonify
+import uuid
+
+users = {}
+categories = {}
+records = {}
 
 @app.route("/healthcheck")
 def healthcheck():
     return {"status": "OK", "message": "Application is healthy"}
+
+@app.route("/user", methods=['POST'])
+def create_user():
+    user_data = request.get_json()
+    user_id = str(uuid.uuid4())
+
+    user = {
+        "id": user_id,
+        "Ім'я": user_data.get("Ім'я")
+    }
+    users[user_id] = user
+    return jsonify(user), 201
+
+
+@app.route('/users', methods=['GET'])
+def get_users():
+    return jsonify(list(users.values())), 200
+
+@app.route('/user/<user_id>', methods=['GET'])
+def get_user(user_id):
+    user = users.get(user_id)
+    if user:
+        return jsonify(user), 200
+    return jsonify({"error": "User not found"}), 404
+
+
+@app.route('/user/<user_id>', methods=['DELETE'])
+def delete_user(user_id):
+    del users[user_id]
+    return '', 204
