44c023809a09ed21ce7aac51eab82363816b3916
Kezaldius <0kezald0@gmail.com>
2025-10-20 13:29:45 +0300

    Created records CRUD

--- api/views.py
+++ api/views.py
@@ -72,10 +72,40 @@ def create_record():
         "id": record_id,
         "Id користувача": record_data.get("user_id"),
         "Id категорії": record_data.get("category_id"),
-        "Дата та час створення запису": datetime.timezone.utc().isoformat(),
+        "Дата та час створення запису": datetime.datetime.now(datetime.timezone.utc).isoformat(),
         "Сума витрати": record_data.get("Сума витрати")
     }
 
     records[record_id] = record
     return jsonify(record), 201
+
+@app.route('/record/<record_id>', methods=['GET'])
+def get_record(record_id):
+    record = records.get(record_id)
+
+    if record:
+        return jsonify(record), 200
+    return jsonify({"error": "Record not found"}), 404
+
+@app.route('/record/<record_id>', methods=['DELETE'])
+def delete_record(record_id):
+    del records[record_id]
+    return '', 204
+
+@app.route('/record', methods=['GET'])
+def get_records():
+    user_id = request.args.get('user_id')
+    category_id = request.args.get('category_id')
+
+    if not user_id and not category_id:
+        return jsonify({"error": "At least one parameter (user_id or category_id) is required"}), 400
+    
+    filtered_records = list(records.values())
+    if user_id:
+        filtered_records = [rec for rec in filtered_records if rec.get("Id користувача") == user_id]
+    if category_id:
+        filtered_records = [rec for rec in filtered_records if rec.get("Id категорії") == category_id]
+
+    return jsonify(filtered_records), 200
+    
 
