1d5c8d1a0f9502ed54f96572ffe131ec6d38c95d
Kezaldius <0kezald0@gmail.com>
2025-11-17 22:25:48 +0200

    Rename jwt

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ class Config:
     OPENAPI_URL_PREFIX = "/"
     OPENAPI_SWAGGER_UI_PATH = "/swagger-ui"
     OPENAPI_SWAGGER_UI_URL = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
-    JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY")
+    JWT = os.environ.get("JWT")
 
     SQLALCHEMY_DATABASE_URI = (
         f"postgresql://{os.environ.get('POSTGRES_USER')}:{os.environ.get('POSTGRES_PASSWORD')}"
