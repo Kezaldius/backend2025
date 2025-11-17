58a76f53b8ffb59824be11b69c6d9b6310fd5b88
Kezaldius <0kezald0@gmail.com>
2025-11-17 18:17:38 +0200

    Add error handling to JWT

--- api/__init__.py
+++ api/__init__.py
@@ -1,5 +1,5 @@
 # api/__init__.py
-from flask import Flask
+from flask import Flask, jsonify
 from flask_smorest import Api
 from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager
@@ -13,6 +13,31 @@ def create_app():
 
     api = Api(app)
     jwt = JWTManager(app)
+
+    @jwt.expired_token_loader
+    def expired_token_callback(jwt_header, jwt_payload):
+        return (
+            jsonify({"message": "Token expired", "error": "token_expired"}),
+            401,
+        )
+
+    @jwt.invalid_token_loader
+    def invalid_token_callback(error):
+        return (
+            jsonify({"message": "Invalid token.", "error": "invalid_token"}),
+            401,
+        )
+
+    @jwt.unauthorized_loader
+    def missing_token_callback(error):
+        return (
+            jsonify({
+                "description": "No token provided.",
+                "error": "authorization_required",
+            }),
+            401,
+        )
+
     db.init_app(app)
     migrate = Migrate(app, db)
 
