346edaaec10576c9ed611b8c50c07a9ae3f86fd3
Kezaldius <0kezald0@gmail.com>
2025-11-17 21:08:57 +0200

    Increase password hash length

--- api/models.py
+++ api/models.py
@@ -9,7 +9,7 @@ class UserModel(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.String(256), nullable=False)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
